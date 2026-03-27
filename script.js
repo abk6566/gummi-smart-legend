178450f720b7061d162be8cfc608e0de7410b56f
abk6566 <dankwantagora6566@gmail.com>
2026-03-27 22:01:48 +0100

    Add form submission handler with alert message

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('regForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Madalla! An karbi rajistarka a Gummi Smart Legend. Barka da shigowa!');
+    this.reset();
+});
+
